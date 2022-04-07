6426e35d6166e87ab1a5b115991659fdc97fbe0a
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2022-04-07 21:43:57 +0300

    First commit

---
+++ common-ui/src/main/java/koropapps/criptoquiz/common_ui/utill/extentions/common.kt
@@ -0,0 +1,11 @@
+package koropapps.criptoquiz.common_ui.utill.extentions
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
